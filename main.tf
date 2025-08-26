474b5f94a85ce7620f0d9aac92198162cd9b19c1
AdirNisim <adirnnisim1@gmail.com>
2025-08-26 11:22:51 +0300

    Added a script for getting http get response, security group for accessing outside to the ec2, ingress mode

--- main.tf
+++ main.tf
@@ -10,7 +10,32 @@ resource "aws_instance" "example" {
   // type of instance memory cpu 
   // documentaion resource:aws_instance for more properties
 
+  // Linking security group with instance below (security group created below)
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  // user data running script at the time of instance launch
+  // EOF lets you to write multi line string
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+  // to replace the instance if user data is changed
+
   tags = {
     Name = "my-ubuntu"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "web"
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
